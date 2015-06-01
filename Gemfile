84090f0cee044b70b6844b9e9fb6c9061bff9a78
Marcos Vinicius Santini <msantini@ciandt.com>
2015-05-31 21:11:24 -0300

    added new action for admin

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development, :test do
   gem 'capybara'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem "better_errors"
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,12 +1,29 @@
 class Admin::AdminsController < Admin::BaseAdminController
 
   def index
-    @admins = Admin.page(params[:page]) 
+    @admins = Admin.page(params[:page])
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new admin_params
+    if @admin.save
+      flash[:notice] = "O Admin \"#{@admin.name}\" foi criado com sucesso !"
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
   
   private
   def set_header
   	@header = "Administradores - #{action_name}"
+  end
+
+  def admin_params
+    params.require(:admin).permit(:name, :email, :password, :password_confirmation)
   end
 end
