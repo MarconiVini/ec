1c3be3475746542790bf683d686fcbb68cb85f4b
Marcos Vinicius Santini <msantini@ciandt.com>
2015-06-06 08:26:03 -0300

    new signout for admin

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -9,6 +9,13 @@ class Admin::SessionsController < Devise::SessionsController
   def sign_up
     @user = User.new
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    case resource_or_scope
+      when :admin then new_admin_session_path
+    end
+  end
+
 end
 
 
