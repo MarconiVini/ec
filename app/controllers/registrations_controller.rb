07db8de9c828fae78f941b78b4e15ad755802079
Marcos Vinicius Cardoso Santini <cardosounicamp@gmail.com>
2016-06-19 09:59:08 -0300

    now is possible to signup on site

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
   layout "login"#, only: [:login, :sign_up]
 
+  before_action :configure_permitted_parameters
+
   def new
     super
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:name)
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path
   end
 
   # def after_sign_out_path_for(resource_or_scope)
