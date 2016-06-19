6a0006dac32d0be8bf82e910d24127d31577ad0a
Marcos Vinicius Cardoso Santini <cardosounicamp@gmail.com>
2016-06-19 10:29:08 -0300

    fix login redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,10 @@ class SessionsController < Devise::SessionsController
     super
   end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   # def after_sign_out_path_for(resource_or_scope)
   #   case resource_or_scope
   #     when :admin then new_admin_session_path
